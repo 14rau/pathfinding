8ca7332d520a565196715af3147b5446806e3487
MasterSeow <chr-rudolph@gmx.de>
2019-04-11 10:57:17 +0200

    added mapscheme as comment

--- Server/PathfindingApi.cs
+++ Server/PathfindingApi.cs
@@ -4,6 +4,14 @@ namespace Server
 {
     class PathfindingApi
     {
+        /* map
+         * 0 = walkable path
+         * 1 = wall
+         * 2 = was agent - deprecated
+         * 3 = starting point
+         * 4 = goal
+         * 5 = fixed wall (not drawable or removable from user -> outer frame)
+         **/
         public static string[] calculatePath(int[][] map)
         {
             String[] movement = { "Up", "Up", "Left" };
