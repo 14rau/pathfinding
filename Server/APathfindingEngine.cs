05e3ec540e36c2aaecde73bb0f43d505a595f5f9
MasterSeow <chr-rudolph@gmx.de>
2019-04-23 13:16:50 +0200

    added options for algorithm

---
+++ Server/APathfindingEngine.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    abstract class APathfindingEngine
+    {
+            public int[][] valueMap;
+            public Agent agent;
+            public Position goalPos;
+            public const int RIGHT = 1;
+            public const int LEFT = 2;
+            public const int UP = 3;
+            public const int DOWN = 4;
+            /* map
+             * 0 = walkable path
+             * 1 = wall
+             * 2 = was agent - deprecated
+             * 3 = starting point
+             * 4 = goal
+             * 5 = fixed wall (not drawable or removable from user -> outer frame)
+             **/
+            public APathfindingEngine(int[][] map, Agent agent)
+            {
+                valueMap = new int[map.Length][];
+                for (int x = 0; x < map.Length; x++)
+                {
+                    valueMap[x] = new int[map[x].Length];
+                    for (int y = 0; y < map[x].Length; y++)
+                    {
+                        if (map[x][y] == 1 || map[x][y] == 5)
+                            valueMap[x][y] = 0;
+                        else
+                            valueMap[x][y] = valueMap.Length + valueMap[0].Length;
+
+                        if (map[x][y] == 3)
+                        {
+                        this.agent = agent;
+                        agent.reset(new Position(x,y));
+                    }
+
+                        if (map[x][y] == 4)
+                        {
+                            goalPos = new Position(x, y);
+                        }
+                    }
+                }
+            }
+
+        public abstract string[] calculatePath();
+          
+
+        public int getBestDirection(int rightValue, int leftValue, int upValue, int downValue)
+            {
+                int movement = 0;
+                int smallest = int.MaxValue;
+
+                if (rightValue > 0 && rightValue < smallest)
+                {
+                    movement = RIGHT;
+                    smallest = rightValue;
+                }
+                if (leftValue > 0 && leftValue < smallest)
+                {
+                    movement = LEFT;
+                    smallest = leftValue;
+                }
+                if (upValue > 0 && upValue < smallest)
+                {
+                    movement = UP;
+                    smallest = upValue;
+                }
+                if (downValue > 0 && downValue < smallest)
+                {
+                    movement = DOWN;
+                    smallest = downValue;
+                }
+                return movement;
+            }
+        }
+    
+
+}
+
--- Server/Program.cs
+++ Server/Program.cs
@@ -27,9 +27,57 @@ namespace Server
             }
 
             var jsonObj = JObject.Parse(text);
-            JArray arr = (JArray)jsonObj["data"];
-            mapArray = arr.ToObject<int[][]>();
-            return PathfindingApi.calculatePath(mapArray);
+            JArray arr = (JArray)jsonObj["map"];
+            int algorithm = (jsonObj["algorithm"] != null)?(int)jsonObj["algorithm"] :0;
+
+            if (jsonObj["settings"] != null) { 
+                JArray settings = (JArray)jsonObj["settings"];
+                mapArray = applyCustomOptions(arr.ToObject<int[][]>(), settings.ToObject<int[]>());
+            }
+            else
+                mapArray = arr.ToObject<int[][]>();
+
+            switch (algorithm)
+            {
+                case 0:
+                    return PathfindingApi.calculatePathRandom(mapArray);
+                case 1:
+                    return PathfindingApi.calculatePathArtem1(mapArray);
+                case 2:
+                    return PathfindingApi.calculatePathArtem2(mapArray);
+                case 3:
+                    return PathfindingApi.calculateArtem3(mapArray);
+                case 4:
+                    return PathfindingApi.calculatePathOwn(mapArray);
+                default:
+                    return PathfindingApi.calculatePathOwn(mapArray);
+
+            }
+
+        }
+
+        private static int[][] applyCustomOptions(int[][] inputMap, int[] settings)
+        {
+            int [][] result = new int[inputMap.Length][];
+            for (int x = 0; x < inputMap.Length; x++)
+            {
+                result[x] = new int[inputMap[x].Length];
+                for (int y = 0; y < inputMap[x].Length; y++)
+                {
+                    if (inputMap[x][y] == settings[0])
+                        result[x][y] = 0;
+
+                    if (inputMap[x][y] == settings[1])
+                        result[x][y] = 1;
+
+                    if (inputMap[x][y] == settings[2])
+                        result[x][y] = 3;
+
+                    if (inputMap[x][y] == settings[3])
+                        result[x][y] = 4;
+                }
+            }
+            return result;
         }
 
     }
