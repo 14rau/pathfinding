d627824a33b91e4b870991060dd3f1df2addbae9
Patrick <pr@tcs.de>
2019-04-29 18:02:19 +0200

    max min width

--- raster/src/components/Editor/Grid/Grid.tsx
+++ raster/src/components/Editor/Grid/Grid.tsx
@@ -16,7 +16,7 @@ export interface IGridProps{
 @observer
 export class Grid extends React.Component<IGridProps>{
     public render() {
-        return <div>
+        return <div style={{maxWidth: "1000px", maxHeight: "1000px",minWidth: "1000px", overflow: "auto"}}>
             {this.props.pageStore.mapData.map((e, y) => <React.Fragment key={y}> {e.map((p, x) => <Tile position={{x,y}} key={`${x}/${y}`} onClick={(e) => this.props.onChange(y, x, this.props.type)} type={p} />)} <br/> </React.Fragment>)}
         </div>
     }
